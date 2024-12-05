9bbe7f2d35fca505358f564a4ccc3c0ce217033f
Y-Ashiq <y.alashiq11@gmail.com>
2024-12-05 22:28:08 +0200

    fix sequelize connection with mysql2

--- database/DBconnection.js
+++ database/DBconnection.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 const sequelize = new Sequelize("sql8750099", process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: "mysql",
+  dialectModule: mysql2
 });
 
 sequelize
